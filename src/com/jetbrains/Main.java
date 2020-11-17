9a6aaad8c39040b5c72a97511ca697306527ab62
Nathalie Flores <21250@student.dorset-college.ie>
2020-11-17 07:28:41 -0600

    do while try and catch and continue loop

--- src/com/jetbrains/Main.java
+++ src/com/jetbrains/Main.java
@@ -1,4 +1,5 @@
 package com.jetbrains;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -6,21 +7,31 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner scanner = new  Scanner (System.in);
-        System.out.println("Please enter an integer numerator/t");
+        boolean value = true;
 
-        int numerator = scanner.nextInt();
+        do {
+            try {
+                System.out.println("");
+                System.out.println("Please enter an integer numerator\t");
 
-        System.out.println("Please enter an integer denominator/t");
-        int denominator = scanner.nextInt();
+                int numerator = scanner.nextInt();
 
-        try {
-            int result = quotient(numerator, denominator);
-            System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, result);
-        }catch (ArithmeticException arithmeticException){
-            System.out.printf("Error ArithmeticException was thrown");
-            System.out.printf("Suggestion: You cannot enter 0");
-        }
+                System.out.println("");
+                System.out.println("Please enter an integer denominator\t");
+                int denominator = scanner.nextInt();
 
+                int result = quotient(numerator, denominator);
+                System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, result);
+                value = false;
+
+            } catch (InputMismatchException inputMismatchException) {
+                System.out.printf("Error Input  was incorrect");
+                System.out.printf("Suggestion: You must enter integer value");
+            } catch (ArithmeticException arithmeticException) {
+                System.out.printf("Error ArithmeticException was thrown");
+                System.out.printf("Suggestion: You cannot enter 0");
+            }
+        }while (value);
 
     }
 
